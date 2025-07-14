0b801f2a0be74c2a750683bf3513d36115daca54
bralcc <cedric.bral@gmail.com>
2025-07-14 20:23:01 +0200

    edited routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\Settings\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+Route::redirect('/', 'dashboard');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
@@ -13,5 +14,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('project', ProjectController::class);
+    Route::resource('task', TaskController::class);
+    Route::resource('profile', ProfileController::class);
+});
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
